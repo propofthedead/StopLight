420274d89156f9330c9200d8e3d9b782d08b637e
Foster Stule <camodude5@yahoo.com>
2018-06-04 10:32:30 -0400

    created a run method and a display traffic light method

--- StopLight/Program.cs
+++ StopLight/Program.cs
@@ -8,7 +8,28 @@ namespace StopLight
 {
 	class Program
 	{
-		void run() { }
+		void DisplayTrafficLight(int color)
+		{
+			switch (color) {
+				case 1:
+					Console.WriteLine("Red");
+					break;
+				case 2:
+					Console.WriteLine("Green");
+					break;
+				case 3:
+					Console.WriteLine("Yellow");
+					break;
+			}
+		}
+		void run() {
+			for (int idx = 0; idx < 3; idx++) {
+				int color = 1;//red
+				DisplayTrafficLight(color);
+				DisplayTrafficLight(++color);
+			}
+
+		}
 
 		static void Main(string[] args)
 		{
